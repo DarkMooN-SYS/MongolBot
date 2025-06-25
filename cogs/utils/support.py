945ee34644ff713a950ec96460aea06340a0f29d
DarkMooN-SYS <liesbolllr123456@gmail.com>
2025-06-25 17:48:14 +0800

    fixed link

--- cogs/utils/support.py
+++ cogs/utils/support.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands
 
 class Support(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command(name="support")
-    async def support(self, ctx):
+    async def support(self, ctx: commands.Context):
         """Ботын тусламж болон албан ёсны серверүүдийн мэдээлэл"""
         embed = discord.Embed(
             title="🤖 Ботын тусламж",
@@ -16,11 +16,11 @@ class Support(commands.Cog):
 
         embed.add_field(name="📜 Командуудын жагсаалт", value="Бүх командуудыг харах бол `mhelp` гэж бичнэ үү.", inline=False)
         embed.add_field(name="🏠 Ботын үндсэн сервер", value="[Энд дарж нэгдээрэй](https://discord.gg/GnVkB37xZS)", inline=False)
-        embed.add_field(name="🛠️ Тусламжийн сервер", value="[Энд дарж нэгдээрэй](https://discord.gg/9ccChND6Zz)", inline=False)
+        embed.add_field(name="🛠️ Тусламжийн сервер", value="[Энд дарж нэгдээрэй](https://discord.gg/UMHWYtnFMA)", inline=False)
         embed.add_field(name="📩 Ботын урилга", value="[Бот нэмэх](https://discord.com/oauth2/authorize?client_id=1268983042425487472&permissions=8&integration_type=0&scope=bot)", inline=False)
         embed.set_footer(text="Хэрэв танд нэмэлт тусламж хэрэгтэй бол тусламжийн серверт нэгдээрэй!")
 
         await ctx.send(embed=embed)
 
-async def setup(bot):
+async def setup(bot: commands.Bot):
     await bot.add_cog(Support(bot))
