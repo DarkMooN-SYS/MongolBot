7848e19771a9ef3067c99f24563dca83445085a9
DarkMooN-SYS <liesbolllr123456@gmail.com>
2025-06-26 15:58:27 +0800

    updated support.py
    removed support server

--- cogs/utils/support.py
+++ cogs/utils/support.py
@@ -16,9 +16,8 @@ class Support(commands.Cog):
 
         embed.add_field(name="📜 Командуудын жагсаалт", value="Бүх командуудыг харах бол `mhelp` гэж бичнэ үү.", inline=False)
         embed.add_field(name="🏠 Ботын үндсэн сервер", value="[Энд дарж нэгдээрэй](https://discord.gg/GnVkB37xZS)", inline=False)
-        embed.add_field(name="🛠️ Тусламжийн сервер", value="[Энд дарж нэгдээрэй](https://discord.gg/UMHWYtnFMA)", inline=False)
         embed.add_field(name="📩 Ботын урилга", value="[Бот нэмэх](https://discord.com/oauth2/authorize?client_id=1268983042425487472&permissions=8&integration_type=0&scope=bot)", inline=False)
-        embed.set_footer(text="Хэрэв танд нэмэлт тусламж хэрэгтэй бол тусламжийн серверт нэгдээрэй!")
+        embed.set_footer(text="Хэрэв танд нэмэлт тусламж хэрэгтэй бол бидэнтэй холбогдоно уу!")
 
         await ctx.send(embed=embed)
 
