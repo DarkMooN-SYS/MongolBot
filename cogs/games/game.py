e12ebd0b31f30684425c29ab54b4ae3878fe8c52
DarkMooN-SYS <liesbolllr123456@gmail.com>
2025-07-10 04:01:31 +0800

    Update game.py

--- cogs/games/game.py
+++ cogs/games/game.py
@@ -348,12 +348,12 @@ class Game(commands.Cog):
             return
         if user_id not in self.win_streaks:
             self.win_streaks[user_id] = 0        # Slots машины алдах магадлалыг 60%-ээс 45% болгох
-        if random.randint(1, 100) <= 50:
+        if random.randint(1, 100) <= 55:
             slot_result = random.sample(["🍒", "💎", "🍌", "🥝", "🎰"], 3)
             self.win_streaks[user_id] = 0
         else:
             three_identical_weights = {
-                "🍒": 40,
+                "🍒": 30,
                 "💎": 20,
                 "🍌": 10,
                 "🥝": 5,
