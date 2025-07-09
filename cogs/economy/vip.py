f5627f937bce8881f18ae5f81a4a978cbbef8e1d
DarkMooN-SYS <liesbolllr123456@gmail.com>
2025-07-10 04:02:07 +0800

    Update vip.py

--- cogs/economy/vip.py
+++ cogs/economy/vip.py
@@ -525,7 +525,7 @@ class VIP(commands.Cog):
         except Exception:
             return await ctx.send("⚠️ Системд алдаа гарлаа!")
 
-    @commands.command(name="giftvip")
+    @commands.command(name="giftvip!!!!!")
     async def giftvip(self, ctx: commands.Context, user: discord.Member) -> Optional[discord.Message]:
         """💳 Найздаа VIP эрх бэлэглэх"""
         if ctx.author.id == user.id:
