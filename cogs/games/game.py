24e630ce6ada478af8c903c0d7283c6985597b0a
DarkMooN-SYS <liesbolllr123456@gmail.com>
2025-07-21 11:41:42 +0800

    updates

--- cogs/games/game.py
+++ cogs/games/game.py
@@ -131,8 +131,8 @@ class MinefieldView(discord.ui.View):
 
     def increase_multiplier(self):
         multiplier_map = {
-            3: [1.2, 1.5, 2, 2.2, 2.5, 3],
-            4: [1.2, 1.5, 2, 2.2, 2.5, 3, 3.2, 3.5, 4, 4.2, 4.5],
+            3: [2.4, 3, 4, 4.4, 5, 6],
+            4: [2.4, 3, 4, 4.4, 5, 6, 6.4, 7, 8, 8.4, 9],
         }
         safe_moves = len(self.opened)
         max_safe_moves = len(multiplier_map[self.dimension])
